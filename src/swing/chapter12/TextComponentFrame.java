4912bdac8382e4d4ab96947ae9862c81784b3a25
Artem Dvornichenko <Artem.Dvornichenko@halliburton.com>
2017-05-12 16:19:46 +0300

    chapter 12 book tasks completed

--- src/swing/chapter12/TextComponentFrame.java
+++ src/swing/chapter12/TextComponentFrame.java
@@ -3,8 +3,6 @@ package swing.chapter12;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
---
+++ src/swing/chapter12/colorChooser/ColorChooserTest.java
@@ -0,0 +1,15 @@
+package swing.chapter12.colorChooser;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ColorChooserTest {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			JFrame frame = new ColorChooserFrame();
+			frame.setTitle("ColorChooserTest");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+		});
+	}
+}
